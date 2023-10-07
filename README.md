2c5850ee1fd25d3d04d2e9f8adaf4f155831d332
Kyung Lee <kyung.lee.official@outlook.com>
2023-10-07 21:25:46 +0800

    Add tests (playwright)

--- README.md
+++ README.md
@@ -3,6 +3,5 @@
 -   Optimize the "Send verification email" button
 -   Update SACL interfaces
 -   Add freeze user logic codes
--   Add tests
 -   Allow update nickname
 -   Allow update user's email
---
+++ tests/role-common.spec.ts
@@ -0,0 +1,38 @@
+import { test, expect } from "@playwright/test";
+import "dotenv/config";
+
+const {
+	E2E_TEST_USER_1_EMAIL,
+	E2E_TEST_USER_1_NICKNAME,
+	E2E_TEST_USER_2_EMAIL,
+	E2E_TEST_USER_2_NICKNAME,
+} = process.env;
+
+test.describe("Test Common Role", () => {
+	test("Test Common Role", async ({ page }) => {
+		/* Sign in page */
+		await page.goto("http://localhost:3000/signin");
+		await page.waitForTimeout(1200); /* Wait for animation */
+		await expect(
+			page.getByRole("heading", { name: "Sign In", exact: true })
+		).toBeVisible();
+		await page
+			.getByRole("heading", { name: "Sign In", exact: true })
+			.hover();
+		await page
+			.getByPlaceholder("Email", { exact: true })
+			.fill(E2E_TEST_USER_1_EMAIL as string);
+		await page
+			.getByPlaceholder("Password", { exact: true })
+			.fill("1234Abcd!");
+		await expect(
+			page.getByRole("button", { name: "Sign In", exact: true })
+		).toBeVisible();
+		await page
+			.getByRole("button", { name: "Sign In", exact: true })
+			.click();
+		await page.waitForTimeout(2000); /* Wait for animation */
+
+		await expect(page.locator(".home-drop-down-menu")).toBeVisible();
+	});
+});
