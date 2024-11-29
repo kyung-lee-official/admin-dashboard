63f18c44d761351203e7f6582db0b58a9cc1897b
Kyung Lee <kyung.lee.official@outlook.com>
2024-11-29 09:55:31 +0800

    style: update date range picker in ctb-docs

---
+++ src/app/app/chitubox-docs-analytics/chitubox-docs-user-feedback/Content.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import dayjs from "dayjs";
+import React, { useEffect, useState } from "react";
+import { useAuthStore } from "@/stores/auth";
+import { Geo } from "@/components/geo/Geo";
+import { useQuery } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+import { usePathname } from "next/navigation";
+import { getChituboxManualFeedbacks } from "@/utils/api/chitubox-manual-feedbacks";
+import { Button } from "@/components/button/Button";
+import { DateRangePicker } from "@/components/date/date-range-picker/DateRangePicker";
+
+export const Content = () => {
+	const pathname = usePathname();
+
+	const { jwt } = useAuthStore();
+	const [isEndBeforeStart, setIsEndBeforeStart] = useState<boolean>(false);
+
+	const [range, setRange] = useState({
+		start: dayjs().startOf("month"),
+		end: dayjs().endOf("month"),
+	});
+
+	const feedbacksQuery = useQuery<any, AxiosError>({
+		queryKey: ["feedbacksQuery", jwt],
+		queryFn: async () => {
+			const feedbacks = await getChituboxManualFeedbacks(
+				range.start,
+				range.end,
+				jwt
+			);
+			return feedbacks;
+		},
+		enabled: true,
+		retry: false,
+		refetchOnWindowFocus: false,
+	});
+
+	const onQuery = () => {
+		feedbacksQuery.refetch();
+	};
+
+	return (
+		<div className="w-full max-w-[1600px] min-h-[calc(100svh-56px)] p-3 gap-y-3">
+			<div
+				className="flex justify-start items-center w-full
+				text-xl"
+			>
+				Select Date Rage
+			</div>
+
+			<div className="flex justify-start items-end gap-3 w-full">
+				<DateRangePicker
+					// onChange={onDateRangeChange}
+					range={range}
+					setRange={setRange}
+				/>
+				<Button
+					size="sm"
+					disabled={isEndBeforeStart}
+					isLoading={feedbacksQuery.isFetching}
+					onClick={onQuery}
+				>
+					Query
+				</Button>
+			</div>
+			<div className="flex justify-start w-full">
+				{/* <Geo feedbacks={feedbacksQuery.data} /> */}
+			</div>
+		</div>
+	);
+};
