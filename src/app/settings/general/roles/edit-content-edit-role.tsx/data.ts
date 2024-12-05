7cc8527912cf72dcdb375a70312d1219cc1ca6e2
Kyung Lee <kyung.lee.official@outlook.com>
2024-12-05 15:09:24 +0800

    feat: add YearPicker
    * add YearPicker

--- src/app/settings/general/roles/edit-content-edit-role.tsx/data.ts
+++
@@ -1,6 +0,0 @@
-/**
- * sort by member name, member must have a 'name' property
- */
-export function sortByMemberName(members: any[]) {
-	return members.sort((a: any, b: any) => a.name.localeCompare(b.name));
-}
