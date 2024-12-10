06ad8453207010642a114068a0930a379b789b3d
Kyung Lee <kyung.lee.official@outlook.com>
2024-12-11 00:41:38 +0800

    chore: move updateIsActive to MenuItems.tsx
    * move updateIsActive to MenuItems.tsx
    * update navMenu

--- src/components/navMenu/NavMenu.tsx
+++ src/components/navMenu/NavMenu.tsx
@@ -9,41 +9,9 @@ import {
 	menuItems,
 	settingsGeneralMenuItems,
 	settingsMyAccountMenuItems,
+	updateIsActive,
 } from "@/components/navMenu/MenuItems";
 import { MoreMenu } from "./moreMenu/MoreMenu";
-
-/**
- * check if the current page is or belongs to the menu by checking its pageUrlReg
- * used to highlight the first two levels of the menu
- * @param m menu
- * @returns boolean
- */
-function updateIsActive(m: HierarchicalMenuItem[]) {
-	const pathname = usePathname();
-	function checkRecursively(m: HierarchicalMenuItem[]): any {
-		let result = false;
-		for (const item of m) {
-			if (item.pageUrlReg) {
-				/* has pageUrlReg */
-				item.isActive = item.pageUrlReg.test(pathname);
-				if (item.isActive) {
-					result = true;
-				}
-			} else {
-				/* no pageUrlReg */
-				if (item.subMenu) {
-					/* has subMenu */
-					result = checkRecursively(item.subMenu);
-					item.isActive = result;
-				} else {
-					/* no subMenu, do nothing */
-				}
-			}
-		}
-		return result;
-	}
-	checkRecursively(m);
-}
 
 export const NavMenuItem = (props: { menu: HierarchicalMenuItem[] }) => {
 	const pathname = usePathname();
@@ -55,39 +23,92 @@ export const NavMenuItem = (props: { menu: HierarchicalMenuItem[] }) => {
 	return (
 		<div>
 			{menu.map((item, i) => {
-				return (
-					<div key={i} className="px-3">
-						<Link
-							href={item.link}
-							className={`flex justify-start items-center h-7 px-2 gap-2.5
-							${item.pageUrlReg?.test(pathname) ? "text-neutral-200" : "text-neutral-400/80"}
-							${
-								item.pageUrlReg?.test(pathname)
-									? "dark:bg-neutral-400/10"
-									: "dark:hover:bg-neutral-400/5"
-							}
-							rounded-md
-							${
-								item.pageUrlReg?.test(pathname) &&
-								"border-[1px] border-white/10 border-t-white/15"
-							}`}
-							title={item.title}
-						>
-							{item.icon && (
-								<div className="flex-[0_0_20px] h-5">
-									{item.icon}
-								</div>
-							)}
-							<div className="min-w-0 text-ellipsis whitespace-nowrap overflow-hidden">
-								{item.title}
-							</div>
-						</Link>
-						{item.subMenu &&
-							item.subMenu.some(
-								(subItem) => subItem.link === pathname
-							) && <NavSubMenuItem menu={item.subMenu} />}
-					</div>
+				const isChildActive = item.subMenu?.some(
+					(subItem) => subItem.isActive
 				);
+				const isActive = item.isActive;
+				if (isChildActive) {
+					/* has subMenu and one of the subMenu items is active */
+					return (
+						<div key={i} className="px-3">
+							<Link
+								href={item.link}
+								className="flex justify-start items-center h-7 px-2 gap-2.5
+								text-neutral-200"
+								title={item.title}
+							>
+								{item.icon && (
+									<div className="flex-[0_0_20px] h-5">
+										{item.icon}
+									</div>
+								)}
+								<div className="min-w-0 text-ellipsis whitespace-nowrap overflow-hidden">
+									{item.title}
+								</div>
+							</Link>
+							{item.subMenu &&
+								/* has subMenu and one of the subMenu is active */
+								item.subMenu.some(
+									(subItem) => subItem.isActive
+								) && <NavSubMenuItem menu={item.subMenu} />}
+						</div>
+					);
+				} else if (isActive) {
+					/* the menu item is active and has no subMenu */
+					return (
+						<div key={i} className="px-3">
+							<Link
+								href={item.link}
+								className="flex justify-start items-center h-7 px-2 gap-2.5
+								text-neutral-200
+								bg-neutral-400/10
+								border-[1px] border-white/10 border-t-white/15"
+								title={item.title}
+							>
+								{item.icon && (
+									<div className="flex-[0_0_20px] h-5">
+										{item.icon}
+									</div>
+								)}
+								<div className="min-w-0 text-ellipsis whitespace-nowrap overflow-hidden">
+									{item.title}
+								</div>
+							</Link>
+							{item.subMenu &&
+								/* has subMenu and one of the subMenu is active */
+								item.subMenu.some(
+									(subItem) => subItem.isActive
+								) && <NavSubMenuItem menu={item.subMenu} />}
+						</div>
+					);
+				} else {
+					/* the menu item is not active and has no subMenu */
+					return (
+						<div key={i} className="px-3">
+							<Link
+								href={item.link}
+								className="flex justify-start items-center h-7 px-2 gap-2.5
+								text-neutral-400/80
+								hover:bg-neutral-400/5"
+								title={item.title}
+							>
+								{item.icon && (
+									<div className="flex-[0_0_20px] h-5">
+										{item.icon}
+									</div>
+								)}
+								<div className="min-w-0 text-ellipsis whitespace-nowrap overflow-hidden">
+									{item.title}
+								</div>
+							</Link>
+							{item.subMenu &&
+								/* has subMenu and one of the subMenu is active */
+								item.subMenu.some(
+									(subItem) => subItem.isActive
+								) && <NavSubMenuItem menu={item.subMenu} />}
+						</div>
+					);
+				}
 			})}
 		</div>
 	);
