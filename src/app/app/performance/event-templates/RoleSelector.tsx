55119eb91dc1e7ced0f10aa23c87b5cd13b26960
Kyung Lee <kyung.lee.official@outlook.com>
2024-12-17 00:07:40 +0800

    feat: replace custom input components with SearchInput for role and member selection

--- src/app/app/performance/event-templates/RoleSelector.tsx
+++ src/app/app/performance/event-templates/RoleSelector.tsx
@@ -1,18 +1,10 @@
 import { MemberRole } from "@/utils/types/internal";
-import { SearchOutlineIcon } from "@/components/icons/Icons";
 import { useAuthStore } from "@/stores/auth";
 import { useQuery } from "@tanstack/react-query";
 import { AxiosError } from "axios";
-import {
-	useState,
-	useRef,
-	useCallback,
-	useEffect,
-	Dispatch,
-	SetStateAction,
-} from "react";
+import { Dispatch, SetStateAction } from "react";
 import { getAllRoles, RolesQK } from "@/utils/api/roles";
-import { sortByProp } from "@/utils/data/data";
+import { SearchInput } from "@/components/input/SearchInput";
 
 type RoleSelectorProps = {
 	role: MemberRole | undefined;
@@ -20,46 +12,6 @@ type RoleSelectorProps = {
 };
 
 export const RoleSelector = (props: RoleSelectorProps) => {
-	const [show, setShow] = useState<boolean>(false);
-
-	const entryRef = useRef<HTMLInputElement>(null);
-	const menuRef = useRef<HTMLDivElement>(null);
-
-	const handleClick = useCallback((e: any) => {
-		if (entryRef.current) {
-			if (
-				e.target === entryRef.current ||
-				entryRef.current.contains(e.target)
-			) {
-				/* entry clicked */
-				setShow((state) => {
-					return !state;
-				});
-			} else {
-				if (menuRef.current) {
-					/* menu clicked */
-					if (
-						e.target === menuRef.current ||
-						menuRef.current.contains(e.target)
-					) {
-						/* do nothing or hide menu, up to you */
-						// setShow(false);
-					} else {
-						/* outside clicked */
-						setShow(false);
-					}
-				}
-			}
-		}
-	}, []);
-
-	useEffect(() => {
-		document.addEventListener("click", handleClick);
-		return () => {
-			document.removeEventListener("click", handleClick);
-		};
-	}, []);
-
 	const jwt = useAuthStore((state) => state.jwt);
 
 	const rolesQuery = useQuery<MemberRole[], AxiosError>({
@@ -75,58 +27,13 @@ export const RoleSelector = (props: RoleSelectorProps) => {
 	const { role, setRole } = props;
 
 	return (
-		<div
-			className="relative flex items-center
-			text-white/50"
-		>
-			<input
-				ref={entryRef}
-				type="text"
-				value={role ? `${role.name} (${role.id})` : ""}
-				title={role ? `${role.name} (${role.id})` : ""}
-				placeholder="Select a role"
-				className="px-2 py-1
-				text-sm
-				bg-white/10
-				border-solid border-l-[1px] border-y-[1px] border-white/10
-				rounded-l-md outline-none whitespace-nowrap text-ellipsis"
-				onChange={() => {}}
-			/>
-			<div
-				className="flex justify-center items-center h-[30px] px-1
-				bg-white/10
-				border-solid border-r-[1px] border-y-[1px] border-white/10
-				rounded-r-md"
-			>
-				<SearchOutlineIcon size={20} />
-			</div>
-			{show && rolesQuery.data && (
-				<div
-					ref={menuRef}
-					className="absolute top-10
-					flex flex-col
-					bg-neutral-800
-					rounded-md shadow-lg border-[1px] border-white/10 border-t-white/15"
-				>
-					{sortByProp(rolesQuery.data, "name").map(
-						(r: MemberRole, i: number) => {
-							return (
-								<button
-									key={r.id}
-									className="flex w-full p-2
-									text-nowrap"
-									onClick={() => {
-										setRole(r);
-										setShow(false);
-									}}
-								>
-									{r.name} ({r.id})
-								</button>
-							);
-						}
-					)}
-				</div>
-			)}
-		</div>
+		<SearchInput
+			selected={role}
+			setSelected={setRole}
+			options={rolesQuery.data ?? []}
+			placeholder="Select a role"
+			labelProp={{ primary: "name", secondary: "id" }}
+			sortBy="name"
+		/>
 	);
 };
--- src/app/app/performance/events/MemberSelector.tsx
+++ src/app/app/performance/events/MemberSelector.tsx
@@ -1,18 +1,10 @@
 import { Member } from "@/utils/types/internal";
-import { SearchOutlineIcon } from "@/components/icons/Icons";
 import { useAuthStore } from "@/stores/auth";
 import { getMembers, MembersQK } from "@/utils/api/members";
 import { useQuery } from "@tanstack/react-query";
 import { AxiosError } from "axios";
-import {
-	useState,
-	useRef,
-	useCallback,
-	useEffect,
-	Dispatch,
-	SetStateAction,
-} from "react";
-import { sortByProp } from "@/utils/data/data";
+import { Dispatch, SetStateAction } from "react";
+import { SearchInput } from "@/components/input/SearchInput";
 
 type MemberSelectorProps = {
 	member: Member | undefined;
@@ -20,46 +12,6 @@ type MemberSelectorProps = {
 };
 
 export const MemberSelector = (props: MemberSelectorProps) => {
-	const [show, setShow] = useState<boolean>(false);
-
-	const entryRef = useRef<HTMLInputElement>(null);
-	const menuRef = useRef<HTMLDivElement>(null);
-
-	const handleClick = useCallback((e: any) => {
-		if (entryRef.current) {
-			if (
-				e.target === entryRef.current ||
-				entryRef.current.contains(e.target)
-			) {
-				/* entry clicked */
-				setShow((state) => {
-					return !state;
-				});
-			} else {
-				if (menuRef.current) {
-					/* menu clicked */
-					if (
-						e.target === menuRef.current ||
-						menuRef.current.contains(e.target)
-					) {
-						/* do nothing or hide menu, up to you */
-						// setShow(false);
-					} else {
-						/* outside clicked */
-						setShow(false);
-					}
-				}
-			}
-		}
-	}, []);
-
-	useEffect(() => {
-		document.addEventListener("click", handleClick);
-		return () => {
-			document.removeEventListener("click", handleClick);
-		};
-	}, []);
-
 	const jwt = useAuthStore((state) => state.jwt);
 
 	const membersQuery = useQuery<Member[], AxiosError>({
@@ -75,57 +27,13 @@ export const MemberSelector = (props: MemberSelectorProps) => {
 	const { member, setMember } = props;
 
 	return (
-		<div
-			className="relative flex items-center
-			text-white/50"
-		>
-			<input
-				ref={entryRef}
-				type="text"
-				value={member ? `${member.name} (${member.email})` : ""}
-				title={member ? `${member.name} (${member.email})` : ""}
-				placeholder="Select a member"
-				className="px-2 py-1
-				text-sm
-				bg-white/10
-				border-solid border-l-[1px] border-y-[1px] border-white/10
-				rounded-l-md outline-none whitespace-nowrap text-ellipsis"
-				onChange={() => {}}
-			/>
-			<div
-				className="flex justify-center items-center h-[30px] px-1
-				bg-white/10
-				border-solid border-r-[1px] border-y-[1px] border-white/10
-				rounded-r-md"
-			>
-				<SearchOutlineIcon size={20} />
-			</div>
-			{show && membersQuery.data && (
-				<div
-					ref={menuRef}
-					className="absolute top-10
-					flex flex-col
-					bg-neutral-800
-					rounded-md shadow-lg border-[1px] border-white/10 border-t-white/15"
-				>
-					{sortByProp(membersQuery.data, "name").map(
-						(m: Member, i: number) => {
-							return (
-								<button
-									key={m.id}
-									className="p-2 text-nowrap"
-									onClick={() => {
-										setMember(m);
-										setShow(false);
-									}}
-								>
-									{m.name} ({m.email})
-								</button>
-							);
-						}
-					)}
-				</div>
-			)}
-		</div>
+		<SearchInput
+			selected={member}
+			setSelected={setMember}
+			options={membersQuery.data ?? []}
+			placeholder="Select a member"
+			labelProp={{ primary: "name", secondary: "email" }}
+			sortBy="name"
+		/>
 	);
 };
